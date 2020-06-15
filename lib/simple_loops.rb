28ab382a9dcb3a4999bf4050cc61d848cfc3b85c
Sam Evans <evansst@icloud.com>
2020-06-15 22:10:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,3 +26,11 @@ def output_array (array)
     counter += 1 
   end
 end
+
+def return_string_array(array)
+  counter = 0 
+  while array[counter] do
+    array[counter].to_s 
+    counter += 1 
+  end
+end
